fed145008219bc575ac337fdda8396eaa354227a
Zyluss <Zyluss@users.noreply.github.com>
2014-08-19 16:18:45 +0100

    Update jquery-code.js

--- files/js/jquery-code.js
+++ files/js/jquery-code.js
@@ -1,7 +1,8 @@
-$(document).load(function (){
-    $("div").hide();
-  });
+var goToURL = null;
 
-$(document).ready(function (){
-    $("div").show();
-  });
+$('a').click(function() {
+   var goToURL = $(this).attr("href");
+     $(el).delay(500 * i).animate({'left': '-300px'}, 1000, function() {window.location = gotoURL;});
+     return false; // or e.preventDefault();
+   }
+);
