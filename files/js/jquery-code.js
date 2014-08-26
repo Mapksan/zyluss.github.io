31aa40b8fcf5a6a0f713345eb4d0664d1d0bdbf6
Zyluss <Zyluss@users.noreply.github.com>
2014-08-26 12:55:58 +0100

    Update jquery-code.js

--- files/js/jquery-code.js
+++ files/js/jquery-code.js
@@ -1,8 +1,3 @@
-var goToURL = null;
-
-$('a').click(function() {
-   var goToURL = $(this).attr("href");
-     $(el).delay(500 * i).animate({'left': '-300px'}, 1000, function() {window.location = gotoURL;});
-     return false; // or e.preventDefault();
-   }
-);
+$(document).ready(function(){
+    $("#current").animate({background-color:'gray'});
+  });
