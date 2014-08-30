c91aaddad1044e7a391e0a75648cada8fc978170
Zyluss <Zyluss@users.noreply.github.com>
2014-08-30 19:07:28 +0100

    Update jquery-code.js

--- files/js/jquery-code.js
+++ files/js/jquery-code.js
@@ -1,3 +1,8 @@
 $(document).unload(function(){
     $("div").animate({background-color:'black'});
   });
+$(document).ready(function(){
+  $("#face").click(function(){
+    document.write("your dead to me.");
+  });
+});
