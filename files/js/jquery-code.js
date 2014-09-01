18de86a5c3f4759bcab6702a460c4cf9ff16972e
Creeper32605 <creeper32605@gmail.com>
2014-09-01 12:35:55 +0200

    pimp up them animations!

--- files/js/jquery-code.js
+++ files/js/jquery-code.js
@@ -1,8 +1,46 @@
 $(document).unload(function(){
-    $("div").animate({background-color:'black'});
+    $("div").animate({background:'black'});
   });
 $(document).ready(function(){
+  
   $("#face").click(function(){
-    document.write("your dead to me.");
+    document.write("you're dead to me.");
   });
+  
+  $("nav a#hover").click(function(e){
+    e.preventDefault();
+    
+    var href = $(this).attr("href");
+    var obj = this;
+    setTimeout(function(){
+      $("body").prepend("<div id='anim'></div>");
+      $("#anim").css("top", $(obj).offset().top )
+      .css("left", $(obj).offset().left )
+      .css("position", "fixed")
+      .css("width", $(obj).innerWidth())
+      .css("height", $(obj).innerHeight())
+      .css("z-index","1000000")
+      .css("background", "#ff9900")
+      .css("transition","all 0.5s cubic-bezier(.2,.3,0,1)")
+      .css("opacity","1");
+    
+      setTimeout(function(){
+        $("#anim").css("top",0)
+        .css("left",0)
+        .css("width","100%")
+        .css("height","100%")
+        .css("opacity",1);
+        setTimeout(function(){
+          if( href == null ) {
+            $("#anim").css("opacity",0);
+            $("#anim").css("pointer-events","none");
+          } else {
+            window.location = href;
+          }
+        },500);
+      },10);
+    },10);
+  
+  });
+  
 });
