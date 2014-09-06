15e1b2c8e63760f2b87dbc70fd66f942fbbd6dcf
Zyluss <Zyluss@users.noreply.github.com>
2014-09-06 09:04:43 +0100

    Create script.js

---
+++ files/js/script.js
@@ -0,0 +1,13 @@
+var black = [255, 50, 05]
+var display = "download";
+var letterColors = [black];
+
+if(10 < 3) {
+    bubbleShape = 'square';
+}
+else {
+    bubbleShape = 'square';
+}
+
+drawName(display, blue);
+bounceBubbles();
