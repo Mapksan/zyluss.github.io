e01bfdf8e7717c83a9d8cc7be8b10993b131681f
Creeps <creeper32605@gmail.com>
2014-09-12 20:48:41 +0200

    nah m8 that won't work

--- files/js/jquery-code.js
+++ files/js/jquery-code.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
   $("#footer").click(function(){
-    $("#footer").css
-     ("paddingTop":"5%",
-     "paddingBottom":"5%"
-    
-     );
+    $("#footer").css(
+    {
+      "padding-top":"5%",
+      "padding-bottom":"5%"
+    });
   });
 });
 
