b9fa1ea20e3d6a1da996fe9d823b6dd4402182f7
Creeps <creeper32605@gmail.com>
2014-09-23 20:30:53 +0200

    good enough :3

---
+++ default.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+
+	//URL query
+	var uq = window.location.search.slice(1); //get the string after ?
+	var uqt = uq.split("&"); //split it into the sections
+	var query = {};
+	//now split it into each of the parts
+	for( var x in uqt ) {
+		query[uqt[x].split("=")[0]] = uqt[x].split("=")[1];
+	}
+	uqt = null; //remove this variable cuz katz
+
+	if( query.h ) {
+		if( query.h.match("\\d+")) {
+			$("#canvas").css("transition","height 0s");
+			setTimeout(function(){
+				var dheight = $("#canvas").height();
+				$("#canvas").css("height", parseInt(query.h)).css("transition","");
+				setTimeout(function(){
+					$("#canvas").css("height",dheight);
+					setTimeout(function(){
+						$("#canvas").css("height","");
+					},500);
+				},2);
+			});
+		}
+	}
+
+	$(".nav").click(function(e){
+		if( e.which == 2 ) {
+			return;
+		}
+		e.preventDefault();
+
+		if( $(this).attr("href") ) {
+			var out = $(this).attr("href");
+			if( $(this).attr("href").substr( $(this).attr("href").length - 1, $(this).attr("href").length ) != "/" ) {
+				out += "/";
+			}
+			document.location = out + "?h=" + Math.round($("#canvas").height()).toFixed(2);
+		}
+
+	});
+
+})
