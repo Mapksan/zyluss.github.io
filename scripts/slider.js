3c2dc9250778a1688a1b132d4647068874577380
Creeps <creeper32605@gmail.com>
2014-09-27 17:25:45 +0200

    fix no-animation as well as formatting

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,46 +1,55 @@
 jQuery(document).ready(function ($) {
-
-  $(document).ready(function(){
-    setInterval(function () {
-        moveRight();
-    }, 5000);
-  });
+    
+  var _int = setInterval(function(){},10000000); //dummy
   
-	var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
-	var sliderUlWidth = slideCount * slideWidth;
-	
-	$('#slider').css({ width: slideWidth, height: slideHeight });
-	
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-	
-    $('#slider ul li:last-child').prependTo('#slider ul');
+  function newInterval() { //used for resetting the setInterval timer to 5000
+    clearInterval(_int);
+    _int = setInterval(function () {
+      moveRight();
+    }, 5000);
+  }
+  
+  $(document).ready(function(){
+    newInterval();
+  });
 
-    function moveLeft() {
-        $('#slider ul').animate({
-            left: + slideWidth
-        }, 200, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
+  var slideCount = $('#slider ul li').length;
+  var slideWidth = $('#slider ul li').width();
+  var slideHeight = $('#slider ul li').height();
+  var sliderUlWidth = slideCount * slideWidth;
 
-    function moveRight() {
-        $('#slider ul').animate({
-            left: - slideWidth
-        }, 200, function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
+  $('#slider').css({ width: slideWidth, height: slideHeight });
 
-    $('a.control_prev').click(function () {
-        moveLeft();
+  $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+  $('#slider ul li:last-child').prependTo('#slider ul');
+
+  function moveLeft() {
+    $('#slider ul').animate({
+        left: + slideWidth
+    }, 200, function () {
+        $('#slider ul li:last-child').prependTo('#slider ul');
+        $('#slider ul').css('left', '');
     });
+  };
 
-    $('a.control_next').click(function () {
-        moveRight();
+  function moveRight() {
+    $('#slider ul').animate({
+        left: - slideWidth
+    }, 200, function () {
+        $('#slider ul li:first-child').appendTo('#slider ul');
+        $('#slider ul').css('left', '');
     });
+  };
+
+  $('a.control_prev').click(function () {
+    moveLeft();
+    newInterval(); //make sure it waits 5 seconds before continuing
+  });
+
+  $('a.control_next').click(function () {
+    moveRight();
+    newInterval(); //make sure it waits 5 seconds before continuing
+  });
 
 });    
