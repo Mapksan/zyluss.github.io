e2268bd16b7638bf90c30181620ff8b3e77b93b8
Creeps <creeper32605@gmail.com>
2014-09-27 18:23:46 +0200

    add interpolation

---
+++ scripts/interpolater.js
@@ -0,0 +1,110 @@
+//parts of the code from http://stackoverflow.com/questions/17360739/jquery-animate-for-element-attributes-not-style
+/*
+ * @param attrs {number} attributes or CSS properties to animate towards
+ * @param speed {number} duration of the animation in milliseconds
+ * @param x1 {number} cubic bezier x1
+ * @param y1 {number} cubic bezier y1
+ * @param x2 {number} cubic bezier x2
+ * @param y2 {number} cubic bezier y2
+ * @param attrmode {boolean} if true, attributes will be animated. If false, CSS properties will be animated
+ */
+$.fn.animateInterpolated = function(attrs, speed, callback, x1, y1, x2, y2, attrmode){
+	
+	var _xnf = this;
+
+	if( $("head").find("#animateInterpolatedWKB").length < 1 ) {
+		$("head").append("<script src='/scripts/webkit-bezier.js' id='animateInterpolatedWKB'>");
+	}
+
+	if( !attrs ) {
+		throw "No attributes defined";
+	}
+
+	x1 = x1 || 0.2;
+	y1 = y1 || 0.3;
+	x2 = x2 || 0.0;
+	y2 = y2 || 1.0;
+
+	// duration in ms
+	speed = speed || 500;
+
+	var start = {}, // object to store initial state of attributes
+		timeout = 5, // interval between rendering loop in ms
+		steps = Math.floor(speed/timeout), // number of cycles required
+		cycles = steps; // counter for cycles left
+
+	// populate the object with the initial state
+	$.each(attrs, function(k,v) {
+		if( attrmode ) {
+			start[k] = _xnf.attr(k);
+		} else {
+			start[k] = parseInt(_xnf.css(k));
+		}
+	});
+	
+	for( var x in start ) {
+		if( isNaN( start[x] ) ) {
+			start[x] = 0;
+		}
+	}
+	
+	(function loop() {
+		for( var x in attrs ) {
+			var k = x;
+			var v = attrs[x];
+			var pst = (v - start[k]) * ( ( Bezier.cubicBezier(.2,.3,0,1, 1 / steps * (steps - cycles), speed) ) );
+			if( attrmode ) {
+				_xnf.attr(k, function(i, old) {
+					return parseInt(start[k]) + pst;
+				});
+			} else {
+				_xnf.css(k, function(i, old) {
+					return parseInt(start[k]) + pst;
+				});
+			}
+		}/*
+	  $.each(attrs, function(k,v) {  // cycle each attribute
+		  /* ORIGINAL CODE
+		  var pst = (v - start[k])/steps;  // how much to add at each step
+		  * /
+
+		  //get interpolated value
+		  var pst = (v - start[k]) * ( ( Bezier.cubicBezier(.2,.3,0,1, 1 / steps * (steps - cycles), speed) ) );
+		  /* ORIGINAL CODE
+		  this.attr(k, function(i, old) {
+			  return +old + pst;  // add value to the old one
+			  return +old + pst;
+		  });
+		  * /
+
+		  if( attrmode ) {
+			  _xnf.attr(k, function(i, old) {
+				  return parseInt(start[k]) + pst;
+			  });
+		  } else {
+			  _xnf.css(k, function(i, old) {
+				  return parseInt(start[k]) + pst; 
+			  });
+		  }
+	  });*/
+
+	  if (--cycles) // call the loop if counter is not exhausted
+		  setTimeout(loop, timeout);
+	  else {// otherwise set final state to avoid floating point values
+		  if( attrmode ) {
+			  _xnf.attr(attrs);
+		  } else {
+			  _xnf.css(attrs);
+		  }
+		  if( callback ) {
+			try {
+			  callback();
+			} catch(err) {
+			  throw err;
+			}
+		  }
+	  }
+
+	})(); // start the loop
+	return this;
+};
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
-    
+  
+  //$("head").append("<script src='/scripts/interpolater.js'>");
+  
   var _int = setInterval(function(){},10000000); //dummy
   
   function newInterval() { //used for resetting the setInterval timer to 5000
@@ -25,21 +27,21 @@ jQuery(document).ready(function ($) {
   $('#slider ul li:last-child').prependTo('#slider ul');
 
   function moveLeft() {
-    $('#slider ul').animate({
+    $('#slider ul').animateInterpolated({
         left: + slideWidth
     }, 200, function () {
         $('#slider ul li:last-child').prependTo('#slider ul');
         $('#slider ul').css('left', '');
-    });
+    }, .2, .3, 0, 1);
   };
 
   function moveRight() {
-    $('#slider ul').animate({
+    $('#slider ul').animateInterpolated({
         left: - slideWidth
     }, 200, function () {
         $('#slider ul li:first-child').appendTo('#slider ul');
         $('#slider ul').css('left', '');
-    });
+    }, .2, .3, 0, 1);
   };
 
   $('a.control_prev').click(function () {
